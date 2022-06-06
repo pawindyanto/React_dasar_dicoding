0527477c12e4a285a9204fa54cf86c8cd523524e
pawindyanto <56341292+pawindyanto@users.noreply.github.com>
2022-06-06 23:51:47 +0700

    Create data.js

---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+const getData = () =>{
+    return [
+        {
+            
+            id: 1,
+            title: "Github",
+            body: "GitHub adalah layanan hos web bersama untuk proyek pengembangan perangkat lunak yang menggunakan sistem kendali versi Git dan layanan hosting internet. Hal ini banyak digunakan untuk kode komputer.",
+            archived: false,
+            createdAt: '2022-06-06T16:29:59.570Z'
+        },
+        {
+            
+            id: 2,
+            title: "Bootstrap",
+            body: "Bootstrap adalah kerangka kerja CSS yang sumber terbuka dan bebas untuk merancang situs web dan aplikasi web. Kerangka kerja ini berisi templat desain berbasis HTML dan CSS untuk tipografi, formulir, tombol, navigasi, dan komponen antarmuka lainnya, serta juga ekstensi opsional JavaScript",
+            archived: false,
+            createdAt: '2022-06-06T16:29:59.570Z'
+        },
+        {
+            
+            id: 3,
+            title: "Taiwind CSS",
+            body: "Tailwind CSS merupakan framework yang bersifat utility-first untuk membangun desain antarmuka khusus dengan cepat. Kebanyakan framework CSS yang lain menyediakan sesuatu secara berlebihan, penyediaan segala jenis komponen yang telah didesain sebelumnya, seperti: buttons, cards, alerts, dan lain sebagainya.",
+            archived: false,
+            createdAt: '2022-06-06T16:29:59.570Z'
+        },
+        {
+            
+            id: 4,
+            title: "React JS",
+            body: "React JS adalah library JavaScript yang biasa digunakan saat membangun UI suatu website atau aplikasi web. Jadi, React JS bisa dianggap seperti perpustakaan yang berisi berbagai kode JavaScript yang sudah tertulis (pre-written). Anda tinggal mengambil kode yang ingin Anda gunakan. Sehingga, ini membuat proses coding menjadi lebih efisien.",
+            archived: false,
+            createdAt: '2022-06-06T16:29:59.570Z'
+        },
+    ];
+
+}
+
+export {getData};
